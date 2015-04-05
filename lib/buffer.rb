f95335e57c9f2a4c3b2d6dc7bf0a41ec5b387df7
Zander Hill <zander@xargs.io>
2015-04-04 23:57:33 -0400

    Use single quotes for style, not performance

--- lib/buffer.rb
+++ lib/buffer.rb
@@ -1,22 +1,22 @@
-require "faraday"
-require "faraday_middleware"
-require "json"
-require "hashie/mash"
-require "addressable/uri"
+require 'faraday'
+require 'faraday_middleware'
+require 'json'
+require 'hashie/mash'
+require 'addressable/uri'
 require 'environs'
 
-require "buffer/version"
-require "buffer/core"
-require "buffer/user"
-require "buffer/profile"
-require "buffer/update"
-require "buffer/link"
-require "buffer/error"
-require "buffer/encode"
-require "buffer/datastructure"
-require "buffer/info"
-require "buffer/client"
-require "buffer/setup"
+require 'buffer/version'
+require 'buffer/core'
+require 'buffer/user'
+require 'buffer/profile'
+require 'buffer/update'
+require 'buffer/link'
+require 'buffer/error'
+require 'buffer/encode'
+require 'buffer/datastructure'
+require 'buffer/info'
+require 'buffer/client'
+require 'buffer/setup'
 
 module Buffer
 end
